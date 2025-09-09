63025f73554096f8d48e42ffa3bf534fb5b4fbc8
Alejandro-Serna17 <aserna17@outlook.com>
2025-09-09 19:41:50 -0400

    Implemented GCD Function

---
+++ students_submissions/gcd_as4384.py
@@ -0,0 +1,10 @@
+# Alejandro Serna CS490 HW2
+
+def gcd(a: int, b: int) -> int:
+    while b != 0:
+        a, b = b, a % b
+    return abs(a)
+
+print(gcd(54, 24))  # Expected output: 6
+print(gcd(48, 18))  # Expected output: 6
+print(gcd(101, 10))  # Expected output: 1
